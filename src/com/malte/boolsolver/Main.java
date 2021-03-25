306271a146948837fea2efe3a97bb88d281188e2
Malte Quandt <MalteQuandt11@gmail.com>
2021-03-25 21:10:19 +0100

    Changed the name

--- src/com/malte/boolsolver/Main.java
+++ src/com/malte/boolsolver/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Main {
     // Solve an boolean expression:
     public static void main(String[] args) {
-        String expression = "a&a|b";
-        // Accepts a simple expression with the 3 simple logical expressions AND (&),
+        String expression = "a&a|b&c";
+        // Accepts a simple expression with the 3 simple logical operators AND (&), OR (|) and NOT (!).
         Solver solver = new Solver(expression);
         solver.solve();
     }
