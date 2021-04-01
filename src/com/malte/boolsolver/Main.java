0d11b990753fd3b04817a15fe3d5f5ccc85f31b1
Malte Quandt <MalteQuandt11@gmail.com>
2021-04-01 22:18:18 +0200

    Finalized the project

--- src/com/malte/boolsolver/Main.java
+++ src/com/malte/boolsolver/Main.java
@@ -3,12 +3,13 @@ package com.malte.boolsolver;
 public class Main {
     // Solve an boolean expression:
     public static void main(String[] args) {
-        String expression = "A+(B*C)";
+        String expression = "a+b+!c+(d*b)*!b*d|!d";
         // Accepts a simple expression with the 3 simple logical operators AND (&, *), OR (|, +) and NOT (!).
         BoolSolver solver = new BoolSolver();
-        solver.eval(expression);
-
+        solver.eval(expression, "trash.tt");
+        System.out.println("Original: \n");
+        System.out.println(expression);
+        System.out.println("Simplified term: \n");
         System.out.println(solver.simplify());
-
     }
 }
