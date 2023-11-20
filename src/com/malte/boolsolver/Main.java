d5e36dae4677b5e49377ac08c59ad7108517ef8d
Malte Quandt <malte.quandt@student.fh-kiel.de>
2023-11-20 16:33:44 +0100

    feat: temporary expression input reading

--- src/com/malte/boolsolver/Main.java
+++ src/com/malte/boolsolver/Main.java
@@ -1,16 +1,31 @@
 package com.malte.boolsolver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     // Solve an boolean expression:
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String expression = "(X1*!X2)|(!X1*X2)|(X4&!X4)&(X3&!X3)";
-        expression = "!A+!A";
+
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+
+        // Read user input
+        String s = br.readLine();
+        System.out.println("Please input a boolean expression: ");
+        if (!s.isEmpty()) {
+            expression = s;
+            System.out.println(expression);
+        }
+
         // Accepts a simple expression with the 3 simple logical operators AND (&, *), OR (|, +) and NOT (!).
         BoolSolver solver = new BoolSolver();
         solver.eval(expression);
         System.out.println("Original:");
         System.out.println(expression);
         System.out.println("Simplified term:");
-        System.out.println(solver.simplify());
+//        System.out.println(solver.simplify());
     }
 }
